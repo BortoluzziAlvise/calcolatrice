212d8179dc6e435b17fe4db99732ec667b800d56
14529936 <14529936@itisrossi.vi.it>
2026-03-24 10:26:16 +0100

    creato file

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
+// main.c
 #include <stdio.h>
+#include "operazioni.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    printf("Somma: %d\n",     somma(4, 3));
+    printf("Differenza: %d\n", differenza(4, 3));
     return 0;
 }
+
---
+++ operazioni.h
@@ -0,0 +1,13 @@
+//
+// Created by 14529936 on 24/03/2026.
+//
+
+#ifndef CALCOLATRICE_OPERAZIONI_H
+#define CALCOLATRICE_OPERAZIONI_H
+
+
+class operazioni {
+};
+
+
+#endif //CALCOLATRICE_OPERAZIONI_H
