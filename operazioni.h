9285d9953f2fbf81191e71e42a2f077e75ab900c
14529936 <14529936@itisrossi.vi.it>
2026-03-24 11:03:30 +0100

    correzioni

--- operazioni.h
+++ operazioni.h
@@ -2,12 +2,9 @@
 // Created by 14529936 on 24/03/2026.
 //
 
-#ifndef CALCOLATRICE_OPERAZIONI_H
-#define CALCOLATRICE_OPERAZIONI_H
-
-
-class operazioni {
-};
-
-
-#endif //CALCOLATRICE_OPERAZIONI_H
+// operazioni.h
+#ifndef OPERAZIONI_H
+#define OPERAZIONI_H
+int somma(int a, int b);
+int differenza(int a, int b);
+#endif
